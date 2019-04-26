ed9f65d42d45e25498cd3f46b87dc008cc0e933a
chittoranjan <chittoranjan.sarker@gmail.com>
2019-04-26 22:28:28 +0600

    Remove support project and add class library

---
+++ PrantiksmeApp/PrantiksmeApp.Models1/Context/PrantiksmeDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PrantiksmeApp.Models.IdentityModels;
+using PrantiksmeApp.Models.Migrations;
+
+namespace PrantiksmeApp.Models.Context
+{
+    public class PrantiksmeDbContext:IdentityDbContext<ApplicationUser, CustomRole, long, CustomUserLogin, CustomUserRole, CustomUserClaim>
+    {
+        public PrantiksmeDbContext() : base("PrantiksmeDbContext")
+        {
+            Configuration.LazyLoadingEnabled = true;
+            Database.CommandTimeout = 1080;
+            ((IObjectContextAdapter)this).ObjectContext.CommandTimeout = 1080;
+
+        }
+        public static PrantiksmeDbContext Create()
+        {
+            return new PrantiksmeDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Entity<ApplicationUser>().ToTable("AppUsers");
+            modelBuilder.Entity<CustomRole>().ToTable("AppRoles");
+            modelBuilder.Entity<CustomUserRole>().ToTable("AppUserRoles");
+            modelBuilder.Entity<CustomUserLogin>().HasKey(c => new { c.UserId, c.ProviderKey, c.LoginProvider }).ToTable("AppUserLogins");
+            modelBuilder.Entity<CustomUserClaim>().ToTable("AppUserClaims");
+
+
+            modelBuilder.Entity<ApplicationUser>().HasKey(c => c.Id);
+            modelBuilder.Entity<ApplicationUser>().Property(c => c.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            modelBuilder.Entity<CustomRole>().HasKey(c => c.Id);
+            modelBuilder.Entity<CustomRole>().Property(c => c.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+    }
+    
+}
